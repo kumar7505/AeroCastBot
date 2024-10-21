04be693f9036a7f9835ee1d4754d53b849adbc08
kumar7505 <kumarpers7@gmail.com>
2024-10-22 00:06:31 +0530

    feat: init

---
+++ index.js
@@ -0,0 +1,24 @@
+// const { Telegraf } = require('telegraf');
+// const bot = new Telegraf('8188467356:AAEmzAy6I8USD0eUjwm8vM-XOv4oTnIyzvo');
+
+// // start command in bot
+// bot.start((ctx) => ctx.reply('Welcome! Your bot is running up'));
+// bot.launch();
+
+
+const express = require( "express");
+express
+process.env.PORT II 4040;
+PORT =
+express();
+app -
+app.use(express.json());
+async (req, res) {
+post");
+app.get("*",
+async (req, res) {
+res.send("Hello get");
+app.listen(PORT, function (err) {
+if (err) console.log(err);
+console. log( "Server listening on PORT" ,
+PORT);
